ee3f803cebb269ab8bedc27e892c04125a23824a
Dinithi Sonali Gamage <75924676+Dini-s@users.noreply.github.com>
2025-03-26 11:02:27 +0530

    implement Charts

---
+++ Frontend/personal-assistance-app/src/Component/UI/AdminDashboard/Revenue/RevenueByServiceChart.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { motion } from "framer-motion";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+export default function RevenueByServiceChart() {
+  return <div>RevenueByServiceChart</div>;
+}
