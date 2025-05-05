6ff89d410dfaad688d245f34bb7dc9fde0e2f86e
Dinithi Gamage <dinithi20000529@gmail.com>
2025-05-05 07:59:39 +0530

    Update SalaryApprovalList.jsx

--- Frontend/personal-assistance-app/src/Component/UI/AdminDashboard/SalaryHandle/SalaryApprovalList.jsx
+++ Frontend/personal-assistance-app/src/Component/UI/AdminDashboard/SalaryHandle/SalaryApprovalList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Spinner, Alert } from "react-bootstrap";
 import { Pagination } from "../Common/Pagination";
 
 export default function SalaryApprovalList() {
