c362b51fd3c9dbd44e2a0bc8f8faf3bf99a7a2b0
Dinithi Gamage <dinithi20000529@gmail.com>
2025-05-07 14:23:47 +0530

    Update review

--- Backend/models/ReviewModel.js
+++ Backend/models/ReviewModel.js
@@ -2,23 +2,20 @@ import mongoose from 'mongoose';
 
 const ReviewSchema = new mongoose.Schema(
   {
-    customerID: { type: mongoose.Types.ObjectId, required: true },
-    providerID: { type: mongoose.Types.ObjectId, required: true },
+    customerID: { type: mongoose.Types.ObjectId, required: true, ref: 'users' },
+    providerID: { type: mongoose.Types.ObjectId, required: true, ref: 'serviceproviders' },
     review: { type: String },
     starRate: { 
       type: Number,
       min: 1,
       max: 5,
-      validate:  {
-        validator: Number.isInteger, 
-        message: 'starRate must be an integer between 1 and 5',
-      },
-    },
+      validate: {
+        validator: Number.isInteger,
+        message: 'Rating must be an integer between 1-5'
+      }
+    }
   },
-  {
-    versionKey: false,
-    timestamps: true
-  },
+  { timestamps: true }
 );
 
 export default mongoose.model('reviews', ReviewSchema);
